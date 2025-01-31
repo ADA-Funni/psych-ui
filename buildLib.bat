4243417f982b32fb8df0283fa7015073b910c91a
ADA_Funni <codingking263@gmail.com>
2025-01-31 17:58:10 +0000

    i have a newfound love for this workflow

--- buildLib.bat
+++ buildLib.bat
@@ -2,7 +2,8 @@ set path="C:\Program Files\WinRAR\";%path%
 del haxelib\*.zip
 mkdir haxelib
 mkdir haxelib\psych-ui
-xcopy  lib\*.* haxelib\psych-ui /e /y
+xcopy lib\*.* haxelib\psych-ui /e /y
+xcopy assets\ui haxelib\psych-ui\assets\ui /e /y
 cd haxelib
 winrar.exe a -afzip psych-ui.zip psych-ui
 haxelib install psych-ui.zip
