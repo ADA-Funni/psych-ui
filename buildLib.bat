246274dfe572552f135ab54a3b1641951a88ab92
ADA_Funni <codingking263@gmail.com>
2025-01-31 18:56:32 +0000

    Revert "e"
    This reverts commit 7a8c269ec182afd25db5c44db916bba2c46d9c0d.

--- buildLib.bat
+++ buildLib.bat
@@ -3,7 +3,7 @@ del haxelib\*.zip
 mkdir haxelib
 mkdir haxelib\psych-ui
 xcopy lib\*.* haxelib\psych-ui /e /y
-xcopy lib\assets\ui haxelib\psych-ui\assets\ui /e /y
+xcopy assets\ui haxelib\psych-ui\assets\ui /e /y
 cd haxelib
 winrar.exe a -afzip psych-ui.zip psych-ui
 haxelib install psych-ui.zip
